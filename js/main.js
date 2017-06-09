64700b99f89dff9167754ade4bc04676a4497dbe
Connie Duong <connieduong@gmail.com>
2017-06-09 08:36:06 -0700

    final update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-var playerOne = 0;
-var playerTwo = 0;
 
-var questions = [
+var game = {score1: 0,
+            score2: 0};
+var turn =1;
+
+// question bank
+
+var questions =[
   {
     question: "Who is the creator of The Big Bang Theory?",
     answers: {
@@ -23,95 +27,93 @@ var questions = [
     correctAnswer: "a"
   },
   {
-    question: "Which of the main characters does not have a Ph’D?",
+    question: "How many times does Sheldon knock on a door?",
     answers: {
-      a: "Leonard Hofstadter",
-      b: "Sheldon Cooper",
-      c: "Rajesh Koothrappli",
-      d: "Howard Wolowitz"
+      a: "2",
+      b: "3",
+      c: "4",
+      d: "5"
+    },
+      correctAnswer: "b"
+  },
+  {
+    question: "Which of the actors in The Big Bang Theory previously worked together on Roseanne?",
+    answers: {
+      a: "Kaley Cucoco, Johnny Galecki, and Melissa Rauch",
+      b: "Johnny Galecki, Sara Gilbert, and Laurie Metcalf.",
+      c: "Jim Parsons, Mayim Bialik, Kunal Nayyar",
+      d: "Simon Helberg, Mellisa Rauch, Kevin Sussman"
+    },
+      correctAnswer: "b"
+  },
+  {
+    question: "Who wrote and recorded the show's theme song?",
+    answers: {
+      a: "Barenaked Ladies",
+      b: "Sublime",
+      c: "Weezer",
+      d: "Nickelback"
+    },
+      correctAnswer: "a"
+  },
+  {
+    question: "Where is Sheldon Cooper from?",
+    answers: {
+      a: "Dallas, TX",
+      b: "Galveston, TX",
+      c: "Houston, TX",
+      d: "Austin, TX"
+    },
+      correctAnswer: "b"
+  },
+  {
+    question: "How old was Sheldon when he earned a PhD?",
+    answers: {
+      a: "14",
+      b: "15",
+      c: "16",
+      d: "17"
+    },
+      correctAnswer: "c"
+  },
+  {
+    question: "To pay her bills, Penny was a waitress what resaurtant?",
+    answers: {
+      a: "California Pizza Kitchen",
+      b: "Cheesecake Factory",
+      c: "P.F. Chang's",
+      d: "Outback Steakhouse"
+    },
+      correctAnswer: "b"
+  },
+  {
+    question: "Which California town does the show take place?",
+    answers: {
+      a: "Sherman Oaks",
+      b: "Hermosa Beach",
+      c: "Pasadena",
+      d: "Irvine"
+    },
+      correctAnswer: "c"
+  },
+  {
+    question: "What is Sheldon's catch phrase?",
+    answers: {
+      a: "Sock it to me.",
+      b: "Yada, Yada, Yada.",
+      c: "Gotcha!",
+      d: "Bazinga!"
     },
       correctAnswer: "d"
   },
-  {
-    question: "question 4, answer b",
-    answers: {
-      a: "answer a",
-      b: "answer b",
-      c: "answer c",
-      d: "answer d"
-    },
-      correctAnswer: "b"
-  },
-  {
-    question: "question 5, answer a",
-    answers: {
-      a: "answer a",
-      b: "answer b",
-      c: "answer c",
-      d: "answer d"
-    },
-      correctAnswer: "a"
-  },
-  {
-    question: "question 6, answer b",
-    answers: {
-      a: "answer a",
-      b: "answer b",
-      c: "answer c",
-      d: "answer d"
-    },
-      correctAnswer: "b"
-  },
-  {
-    question: "question 7, answer c",
-    answers: {
-      a: "answer a",
-      b: "answer b",
-      c: "answer c",
-      d: "answer d"
-    },
-      correctAnswer: "c"
-  },
-  {
-    question: "question 8, answer a",
-    answers: {
-      a: "answer a",
-      b: "answer b",
-      c: "answer c",
-      d: "answer d"
-    },
-      correctAnswer: "a"
-  },
-  {
-    question: "question 9, answer c",
-    answers: {
-      a: "answer a",
-      b: "answer b",
-      c: "answer c",
-      d: "answer d"
-    },
-      correctAnswer: "c"
-  },
-  {
-    question: "question 10, answer a",
-    answers: {
-      a: "answer a",
-      b: "answer b",
-      c: "answer c",
-      d: "answer d"
-    },
-      correctAnswer: "a"
-  }
 ]
 
 var randomQuestion = Math.floor(Math.random() *questions.length);
 
-
 function showQuestion() {
     var showQuestion = (questions[randomQuestion].question);
     var $showQuestion = showQuestion;
-    var $quiz = '#quiz';
-    $(quiz).append($showQuestion);
+    $('#quiz').html($showQuestion);
   }
 showQuestion ()
 
@@ -130,11 +132,12 @@ function removeQuestion(index) {
 }
 
 
+
 function showAnswers () {
-$(answerChoices).html("<button class='answerButton' data-answerid='a'>" + questions[randomQuestion].answers.a + "</button>");
-$(answerChoices).append("<button class='answerButton' data-answerid='b'>" + questions[randomQuestion].answers.b + "</button>");
-$(answerChoices).append("<button class='answerButton' data-answerid='c'>" + questions[randomQuestion].answers.c + "</button>");
-$(answerChoices).append("<button class='answerButton' data-answerid='d'>" + questions[randomQuestion].answers.d + "</button>");
+$('#a').html("<button class='answerButton' data-answerid='a'>"+questions[randomQuestion].answers.a + "</button>");
+$('#b').html("<button class='answerButton' data-answerid='b'>"+questions[randomQuestion].answers.b + "</button>");
+$('#c').html("<button class='answerButton' data-answerid='c'>"+questions[randomQuestion].answers.c + "</button>");
+$('#d').html("<button class='answerButton' data-answerid='d'>"+questions[randomQuestion].answers.d + "</button>");
   $(".answerButton").click(function() {
         console.log("You clicked answer: " + $(this).data("answerid"))
         checkAnswers($(this).data("answerid"))
@@ -143,32 +146,46 @@ $(answerChoices).append("<button class='answerButton' data-answerid='d'>" + ques
 showAnswers()
 
 function checkAnswers(answerLetter) {
-    if ( answerLetter === questions[randomQuestion].correctAnswer)
-      {alert("Correct answer!")
-      playerOne = playerOne + 1;
-      $('#playerOne').html("Player One Score: " + playerOne);
-      // console.log("before delete", randomQuestion, questions);
-      //console.log("randomQuestion", randomQuestion);
+    if ( answerLetter === questions[randomQuestion].correctAnswer) {
+      alert("Correct answer!");
+
+      if (turn === 1) {
+        game.score1 = game.score1 + 1;
+        $('#playerOne').html("Player One Score: " + game.score1);
+      }
+
+      if (turn === 2) {
+        game.score2 = game.score2 + 1;
+        $('#playerTwo').html("Player Two Score: " + game.score2);
+      }
+
       removeQuestion(questions[randomQuestion]);
-      // console.log("after delete", questions);
-      nextQuestion();
-    } else
-    {
+      // nextQuestion();
+    } else {
       alert("Wrong answer.")
       removeQuestion(questions[randomQuestion]);
-      nextQuestion();
+      // nextQuestion();
     }
-  }
+    switchTurns()
+    endGame()
+nextQuestion()
+}
+
+function keepScore (){
+  $('#playerOne').html("Player One Score: " + game.score1);
+  $('#playerTwo').html("Player Two Score: " + game.score2);
+}
+keepScore ();
 
 function nextQuestion() {
   console.log("next question")
-    var $quiz = '#quiz'
-    var $showQuestion = showQuestion
-    $(quiz).append($showQuestion);
-    $('#answerChoices').html("<button class='answerButton' data-answerid='a'>" + questions[randomQuestion].answers.a + "</button>");
-    $('#answerChoices').append("<button class='answerButton' data-answerid='b'>" + questions[randomQuestion].answers.b + "</button>");
-    $('#answerChoices').append("<button class='answerButton' data-answerid='c'>" + questions[randomQuestion].answers.c + "</button>");
-    $('#answerChoices').append("<button class='answerButton' data-answerid='d'>" + questions[randomQuestion].answers.d + "</button>");
+  randomQuestion = Math.floor(Math.random() *questions.length);
+    var $showQuestion = showQuestion;
+    $('#quiz').append($showQuestion);
+    $('#a').html("<button class='answerButton' data-answerid='a'>" + questions[randomQuestion].answers.a + "</button>");
+    $('#b').html("<button class='answerButton' data-answerid='b'>" + questions[randomQuestion].answers.b + "</button>");
+    $('#c').html("<button class='answerButton' data-answerid='c'>" + questions[randomQuestion].answers.c + "</button>");
+    $('#d').html("<button class='answerButton' data-answerid='d'>" + questions[randomQuestion].answers.d + "</button>");
 
     $(".answerButton").click(function() {
           console.log("You clicked answer: " + $(this).data("answerid"))
@@ -176,18 +193,22 @@ function nextQuestion() {
   })
 }
 
+function switchTurns() {
+  if (turn ===1 ){
+    turn = 2;
+  } else {
+    turn = 1
+  }
+}
 
-
-
-// event listener to load next question
-
-
-// function switchTurns() {
-//   var currentPlayer = playerOne;
-//     if(currentPlayer == playerOne)
-//     {alert("Next player's turn");
-//         currentPlayer = playerTwo
-//     } else {
-//     currentPlayer = playerOne
-//     }
-//   }
+function endGame() {
+  if (game.score1 === 3 || game.score2 ===3) {
+    if (game.score1 > game.score2){
+      alert("Player 1 won")
+      return false;
+    } else {
+      alert("Player 2 won")
+      return false;
+    }
+  }
+}
